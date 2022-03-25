800ba8a39a5253150912d2fc3af26483cccf84f8
selgueti <sofian.elguetibi@gmail.com>
2022-03-25 22:39:33 +0100

    work on ClientChatFusion

--- src/fr/uge/net/chatFusion/ClientChatFusion.java
+++ src/fr/uge/net/chatFusion/ClientChatFusion.java
@@ -1,4 +1,294 @@
 package fr.uge.net.chatFusion;
 
+import fr.uge.net.chatFusion.reader.MessageReader;
+import fr.uge.net.chatFusion.reader.Reader;
+import fr.uge.net.chatFusion.token.Message;
+import fr.uge.net.chatFusion.util.StringController;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class ClientChatFusion {
+
+    static private final int BUFFER_SIZE = 10_000;
+    static private final Logger logger = Logger.getLogger(ClientChatFusion.class.getName());
+    private final SocketChannel sc;
+    private final Selector selector;
+    private final InetSocketAddress serverAddress;
+    private final String login;
+    private final Thread console;
+    private final StringController stringController = new StringController();
+    private Context uniqueContext;
+
+    public ClientChatFusion(String login, InetSocketAddress serverAddress) throws IOException {
+        this.serverAddress = serverAddress;
+        this.login = login;
+        this.sc = SocketChannel.open();
+        this.selector = Selector.open();
+        this.console = new Thread(this::consoleRun);
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        if (args.length != 3) {
+            usage();
+            return;
+        }
+        new ClientChatFusion(args[0], new InetSocketAddress(args[1], Integer.parseInt(args[2]))).launch();
+    }
+
+    private static void usage() {
+        System.out.println("Usage : ClientChatFusion login hostname port");
+    }
+
+    private void consoleRun() {
+        try {
+            try (var scanner = new Scanner(System.in)) {
+                while (scanner.hasNextLine()) {
+                    var msg = scanner.nextLine();
+                    sendCommand(msg);
+                }
+            }
+            logger.info("Console thread stopping");
+        } catch (InterruptedException e) {
+            logger.info("Console thread has been interrupted");
+        }
+    }
+
+    /**
+     * Send instructions to the selector via messageController and wake it up
+     *
+     * @param msg - msg
+     * @throws InterruptedException - if thread has been interrupted
+     */
+    private void sendCommand(String msg) throws InterruptedException {
+        stringController.add(msg);
+        selector.wakeup();
+        // Cause the exception if the main thread has requested the interrupt
+        if (Thread.interrupted()) {
+            throw new InterruptedException("Interrupted by main thread");
+        }
+    }
+
+    /**
+     * Processes the command from the messageController
+     */
+    private void processCommands() {
+        while (stringController.hasString()) {
+            uniqueContext.queueMessage(new Message(login, stringController.poll()));
+        }
+    }
+
+    public void launch() throws IOException {
+        sc.configureBlocking(false);
+        var key = sc.register(selector, SelectionKey.OP_CONNECT);
+        uniqueContext = new Context(key);
+        key.attach(uniqueContext);
+        sc.connect(serverAddress);
+
+        console.start();
+
+        while (!Thread.interrupted()) {
+            try {
+                selector.select(this::treatKey);
+                processCommands();
+            } catch (UncheckedIOException tunneled) {
+                console.interrupt();
+                Thread.currentThread().interrupt();
+                throw tunneled.getCause();
+            }
+        }
+    }
+
+    private void treatKey(SelectionKey key) {
+        // TODO review the treatment of exceptions from method doConnect, doRead, doWrite
+        try {
+            if (key.isValid() && key.isConnectable()) {
+                uniqueContext.doConnect();
+            }
+        } catch (IOException ioe) {
+            // lambda call in select requires to tunnel IOException
+            logger.warning(serverAddress.getHostString() + ":" + serverAddress.getPort() + " is unreachable");
+            silentlyClose(key);
+            console.interrupt();
+            Thread.currentThread().interrupt();
+            //throw new UncheckedIOException(ioe);
+        }
+
+        try {
+            if (key.isValid() && key.isWritable()) {
+                uniqueContext.doWrite();
+            }
+        } catch (IOException ioe) {
+            // lambda call in select requires to tunnel IOException
+            logger.info("I/O error while sending a message");
+            //silentlyClose(key);
+            //throw new UncheckedIOException(ioe);
+        }
+
+        try {
+            if (key.isValid() && key.isReadable()) {
+                uniqueContext.doRead();
+            }
+        } catch (IOException ioe) {
+            // lambda call in select requires to tunnel IOException
+            logger.info("I/O error while receiving a message");
+            //silentlyClose(key);
+            //throw new UncheckedIOException(ioe);
+        }
+    }
+
+    private void silentlyClose(SelectionKey key) {
+        Channel sc = key.channel();
+        try {
+            sc.close();
+        } catch (IOException e) {
+            // ignore exception
+        }
+    }
+
+    static private class Context {
+        private final SelectionKey key;
+        private final SocketChannel sc;
+        private final ByteBuffer bufferIn = ByteBuffer.allocate(BUFFER_SIZE);
+        private final ByteBuffer bufferOut = ByteBuffer.allocate(BUFFER_SIZE);
+        private final MessageReader messageReader = new MessageReader();
+        private final ArrayDeque<Message> queue = new ArrayDeque<>();
+        private boolean closed = false;
+
+        private Context(SelectionKey key) {
+            this.key = key;
+            this.sc = (SocketChannel) key.channel();
+        }
+
+        /**
+         * Process the content of bufferIn
+         * <p>
+         * The convention is that bufferIn is in write-mode before the call to process
+         * and after the call
+         */
+        private void processIn() {
+            for (; ; ) {
+                Reader.ProcessStatus status = messageReader.process(bufferIn);
+                switch (status) {
+                    case DONE:
+                        var message = messageReader.get();
+                        System.out.println(message);
+                        messageReader.reset();
+                        break;
+                    case REFILL:
+                        return;
+                    case ERROR:
+                        silentlyClose();
+                        return;
+                }
+            }
+        }
+
+        /**
+         * Add a message to the message queue, tries to fill bufferOut and updateInterestOps
+         *
+         * @param msg - msg
+         */
+        private void queueMessage(Message msg) {
+            queue.addLast(msg);
+            processOut();
+            updateInterestOps();
+        }
+
+        /**
+         * Try to fill bufferOut from the message queue
+         */
+        private void processOut() {
+            while (!queue.isEmpty()) {
+                var message = queue.peekFirst();
+                message.fillBuffer(bufferOut);
+                if (message.isDone()) {
+                    queue.removeFirst();
+                }
+            }
+        }
+
+        /**
+         * Update the interestOps of the key looking only at values of the boolean
+         * closed and of both ByteBuffers.
+         * <p>
+         * The convention is that both buffers are in write-mode before the call to
+         * updateInterestOps and after the call. Also, it is assumed that process has
+         * been be called just before updateInterestOps.
+         */
+
+        private void updateInterestOps() {
+            var interestOps = 0;
+            if (!key.isValid()) {
+                return;
+            }
+            if (!closed && bufferIn.hasRemaining()) {
+                interestOps |= SelectionKey.OP_READ;
+            }
+            if (bufferOut.position() != 0) {
+                interestOps |= SelectionKey.OP_WRITE;
+            }
+            if (interestOps == 0) {
+                silentlyClose();
+                return;
+            }
+            key.interestOps(interestOps);
+        }
+
+        private void silentlyClose() {
+            try {
+                sc.close();
+            } catch (IOException e) {
+                // ignore exception
+            }
+        }
+
+        /**
+         * Performs the read action on sc
+         * <p>
+         * The convention is that both buffers are in write-mode before the call to
+         * doRead and after the call
+         *
+         * @throws IOException - If an I/O error occurs
+         */
+        private void doRead() throws IOException {
+            if (-1 == sc.read(bufferIn)) {
+                closed = true;
+            }
+            processIn();
+            updateInterestOps();
+        }
+
+        /**
+         * Performs the write action on sc
+         * <p>
+         * The convention is that both buffers are in write-mode before the call to
+         * doWrite and after the call
+         *
+         * @throws IOException - If an I/O error occurs
+         */
+
+        private void doWrite() throws IOException {
+            bufferOut.flip();
+            sc.write(bufferOut);
+            bufferOut.compact();
+            processOut();
+            updateInterestOps();
+        }
+
+        public void doConnect() throws IOException {
+            if (!sc.finishConnect())
+                return; // the selector gave a bad hint
+            key.interestOps(SelectionKey.OP_READ);
+        }
+    }
 }
