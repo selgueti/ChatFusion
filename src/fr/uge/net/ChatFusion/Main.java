4e686e439cf93e66834f7881b12b80c02c9e32c9
selgueti <sofian.elguetibi@gmail.com>
2022-03-25 21:06:16 +0100

    Initial commit

---
+++ src/fr/uge/net/ChatFusion/Main.java
@@ -0,0 +1,8 @@
+package fr.uge.net.ChatFusion;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ChatFusion, java : " + System.getProperty("java.version"));
+    }
+}
