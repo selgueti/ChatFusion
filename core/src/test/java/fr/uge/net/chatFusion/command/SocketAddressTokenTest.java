58871eacab02a52f57ed8305af0c648c0a4f0acd
selgueti <sofian.elguetibi@gmail.com>
2022-04-24 01:50:24 +0200

    add SocketAddressTokenTest

---
+++ core/src/test/java/fr/uge/net/chatFusion/command/SocketAddressTokenTest.java
@@ -0,0 +1,23 @@
+package fr.uge.net.chatFusion.command;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.InetSocketAddress;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SocketAddressTokenTest {
+
+    public void toBufferIsInWriteMode() {
+        SocketAddressToken socketAddressToken = new SocketAddressToken(new InetSocketAddress("localhost", 7777).getAddress(), 7777);
+        assertTrue(socketAddressToken.toBuffer().position() != 0);
+    }
+
+    @Test
+    void checkBufferLength() {
+        SocketAddressToken socketAddressToken = new SocketAddressToken(new InetSocketAddress("localhost", 7777).getAddress(), 7777);
+        var length = Byte.BYTES + Byte.BYTES * socketAddressToken.address().getAddress().length + Integer.BYTES;
+        assertEquals(length, socketAddressToken.toBuffer().position());
+    }
+}
