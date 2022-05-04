ac34661fef8076f603dcf3a479a3fee47e88e5de
hpetiot <hm.petiot@gmail.com>
2022-05-04 02:16:20 +0200

    Save commit too tried to keep on

--- core/src/main/java/fr/uge/net/chatFusion/util/FileSendInfo.java
+++ core/src/main/java/fr/uge/net/chatFusion/util/FileSendInfo.java
@@ -6,33 +6,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileSendInfo {
-    private final BufferedReader file;
+    private final FileChannel file;
     private final String loginDst;
     private final String fileName;
     private final String serverDst;
     private int progress = 0;
     private static final int MAX_IN_COMMAND = 5000;
     private final int nbChunk;
-    private char[] charBuff = new char[MAX_IN_COMMAND];
-
+    private ByteBuffer chunk = ByteBuffer.allocate(MAX_IN_COMMAND);
     public FileSendInfo(String filePath, String loginDst, String serverDst, String fileName) throws IOException {
-        file  = Files.newBufferedReader(Path.of(filePath), StandardCharsets.US_ASCII);
-        nbChunk = Math.toIntExact(Files.size(Path.of(filePath)) / MAX_IN_COMMAND);
+        Path path = Path.of(filePath);
+        file  = FileChannel.open(path);
+        nbChunk = Math.toIntExact(file.size()) / MAX_IN_COMMAND);
         this.fileName = fileName;
         this.loginDst = loginDst;
         this.serverDst = serverDst;
     }
 
-    public ByteBuffer buildFieChunk(ClientChatFusion client) throws IOException {
-        var data = readChunkIntoCommand();
+    public ByteBuffer buildFileChunk(ClientChatFusion client) throws IOException {
+        var readValue = 1;
+        while(chunk.hasRemaining()){
+            readValue = file.read(chunk);
+            if(readValue <= 0){
+
+            }
+        }
         return client.buildFileChunk(data, serverDst, loginDst, fileName, nbChunk);
     }
-
-    public byte[] readChunkIntoCommand() throws IOException {
-
-}
